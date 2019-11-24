864b9c86d55e1864352383110a434d490ca590ed
Barbara1998 <barbara.germaneau@gmail.com>
2019-11-24 22:27:24 +0100

    update

--- 02_typescript/src/metrics.ts
+++ 02_typescript/src/metrics.ts
@@ -8,12 +8,12 @@ export class Metric {
     }
   }
   
-  export class MetricsHandler {
-    static get(callback: (error: Error | null, result?: Metric[]) => void) {
-      const result = [
-        new Metric('2013-11-04 14:00 UTC', 12),
-        new Metric('2013-11-04 14:30 UTC', 15)
-      ]
-      callback(null, result)
-    }
+export class MetricsHandler {
+  static get(callback: (error: Error | null, result?: Metric[]) => void) {
+    const result = [
+      new Metric('2013-11-04 14:00 UTC', 12),
+      new Metric('2013-11-04 14:30 UTC', 15)
+    ]
+    callback(null, result)
   }
+}
--- 02_typescript/src/server.ts
+++ 02_typescript/src/server.ts
@@ -1,9 +1,17 @@
+//import express module
 import express = require('express')
-
+//create a server
 const app = express()
 const port: string = process.env.PORT || '8080'
 
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public'))) 
+
+app.set('views', __dirname + "./view");
+app.set('view engine', 'ejs');
+
 import { MetricsHandler } from './metrics'
+
 app.get('/metrics.json', (req: any, res: any) => {
   MetricsHandler.get((err: Error | null, result?: any) => {
     if (err) {
@@ -17,6 +25,22 @@ app.get('/', (req: any, res: any) => {
   res.write('Hello world')
   res.end()
 })
+
+//don't know how to render html with typescript 
+// app.get('/hello', (req: any, res: any) => {
+// //   res.write('<html lang="en"> '+
+// //  '<body class="container">'+
+// //       '<div class="col-md-6 col-md-offset-3">'+
+// //           '<h1>Hello </h1>'+
+// //           '<button class="btn btn-success" id="show-metrics">'+
+// //           'Bring the metrics'+
+// //           '</button>'+
+// //           '<div id="metrics"></div>'+
+// //       '</div>'+
+// //   '</body>'+
+// // '</html>')
+//   res.end()
+// })
 
 app.listen(port, (err: Error) => {
   if (err) {
