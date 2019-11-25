67528ffd5f3596f468fa4b8faec73b7a5e602ea2
Barbara1998 <barbara.germaneau@gmail.com>
2019-11-25 17:20:34 +0100

    update and creation of new folder

--- 02_typescript/src/server.ts
+++ 02_typescript/src/server.ts
@@ -1,16 +1,17 @@
 //import express module
 import express = require('express')
+import { MetricsHandler } from './metrics'
+import path = require('path')
+
 //create a server
 const app = express()
 const port: string = process.env.PORT || '8080'
 
-const path = require('path')
 app.use(express.static(path.join(__dirname, 'public'))) 
 
-app.set('views', __dirname + "./view");
+app.set('views', __dirname + "/view");
 app.set('view engine', 'ejs');
 
-import { MetricsHandler } from './metrics'
 
 app.get('/metrics.json', (req: any, res: any) => {
   MetricsHandler.get((err: Error | null, result?: any) => {
@@ -26,21 +27,10 @@ app.get('/', (req: any, res: any) => {
   res.end()
 })
 
-//don't know how to render html with typescript 
-app.get('/hello', (req: any, res: any) => {
-  res.write('<html lang="en"> '+
- '<body class="container">'+
-      '<div class="col-md-6 col-md-offset-3">'+
-          '<h1>Hello </h1>'+
-          '<button class="btn btn-success" id="show-metrics">'+
-          'Bring the metrics'+
-          '</button>'+
-          '<div id="metrics"></div>'+
-      '</div>'+
-  '</body>'+
-'</html>')
-  res.end()
-})
+app.get(
+  '/hello/:name', 
+  (req, res) => res.render('hello.ejs', {name: req.params.name})
+)
 
 app.listen(port, (err: Error) => {
   if (err) {
